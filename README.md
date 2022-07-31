d83cbe3604118e5d3a2ab464e29192ff6929f884
Stefan <sbackovic93@gmail.com>
2022-07-31 14:29:07 +0200

    Create project

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringNoSQL
+Integration of different NoSQL databases to Spring Boot project
---
+++ src/test/java/com/ck0ba/springnosql/SpringNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck0ba.springnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
